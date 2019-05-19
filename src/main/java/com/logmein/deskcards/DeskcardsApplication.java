39543ca0bd511f376a3886f4c5d1a1b8d26c9b11
Fabrice Touopi <bruneltouopi@yahoo.fr>
2019-05-19 04:45:34 +0200

    init

---
+++ src/main/java/com/logmein/deskcards/DeskcardsApplication.java
@@ -0,0 +1,13 @@
+package com.logmein.deskcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeskcardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logmein/deskcards/DeskcardsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.logmein.deskcards;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeskcardsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
